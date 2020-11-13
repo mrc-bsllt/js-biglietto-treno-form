d867949ea47aa4d82a3cab2be16ac14b20aca36f
MarcoB <mrc.bsllt@gmail.com>
2020-11-13 15:17:10 +0100

    completato logica del prezzo

--- js/script.js
+++ js/script.js
@@ -5,12 +5,27 @@ function() {
 
   // estraggo le informazioni inserite dall'utente
   var nomeUtente = document.getElementById("user_name").value;
-  console.log(nomeUtente);
   var kmUtente = parseInt(document.getElementById("user_km").value);
-  console.log(kmUtente);
   var etaUtente = document.getElementById("user_age").value;
-  console.log(etaUtente);
-  
+
+  // calcolo i possibili prezzi dei biglietti
+  var prezzoStandardBiglietto = kmUtente * 0.21;
+  var prezzoMinorenniBiglietto = prezzoStandardBiglietto - (prezzoStandardBiglietto * 0.2);
+  var prezzoOverBiglietto = prezzoStandardBiglietto - (prezzoStandardBiglietto * 0.4);
+  var prezzoFinale = "";
+
+  if (etaUtente == "Minorenne") {
+    prezzoFinale = prezzoMinorenniBiglietto.toFixed(2);
+  } else if (etaUtente == "Over 65") {
+    prezzoFinale = prezzoOverBiglietto.toFixed(2);
+  } else {
+    prezzoFinale = prezzoStandardBiglietto.toFixed(2);
+  }
+
+  document.getElementById("ticket_price").innerHTML = prezzoFinale + " €";
+
+  // scrivo a video le informazione che ha inserito l'utente
+  var nomeUtenteValore = document.getElementById("name_value").innerHTML = nomeUtente;
 }
 
 );
